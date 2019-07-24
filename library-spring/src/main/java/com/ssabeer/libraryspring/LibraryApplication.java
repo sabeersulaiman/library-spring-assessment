6244428aad469bc54b02c81e47cc77d711b45789
Sabeer Sulaiman <sabeersulaimanpv@gmail.com>
2019-07-24 15:17:31 +0530

    feature(base): adding CORS support

--- library-spring/src/main/java/com/ssabeer/libraryspring/LibraryApplication.java
+++ library-spring/src/main/java/com/ssabeer/libraryspring/LibraryApplication.java
@@ -4,17 +4,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssabeer.libraryspring.models.Book;
 import com.ssabeer.libraryspring.models.enums.BookStatus;
 import com.ssabeer.libraryspring.services.BookService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableTransactionManagement
 public class LibraryApplication {
+	@Value("${library.cors.urls}")
+	private String corsUrls;
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList(corsUrls));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 	@EventListener
 	public void seedBooks(ContextRefreshedEvent event) {
 		// seed the books
